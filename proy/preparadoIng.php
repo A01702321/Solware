4c4b67b532e24f6957120c7fbfe0467e3d438b23
manuelflrs <32782300+manuelflrs@users.noreply.github.com>
2019-10-30 01:07:57 -0600

    agregar preparado e ing sirviendo con base de datos

--- proy/preparadoIng.php
+++ proy/preparadoIng.php
@@ -4,6 +4,7 @@ server with default setting (user 'root' with no password) */
 
 $link = mysqli_connect('localhost', 'root', '', 'clase');
 $name = $_POST['name'];
+$ing = $_POST['ingredient'];
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
@@ -11,7 +12,8 @@ if($link === false){
  
 // Attempt insert query execution
 $sql = "
-INSERT INTO Preparados (Nombre) VALUES ('$name')";
+INSERT INTO Conforman (IDPreparado, IDIngrediente) VALUES ((SELECT p.IDPreparado FROM Preparados p WHERE p.Nombre = '$name'), (SELECT z.IDIngrediente FROM Ingredientes z WHERE z.Nombre = '$ing' ) )";
+
 
 if(mysqli_query($link, $sql)){
     echo "Records inserted successfully.";
