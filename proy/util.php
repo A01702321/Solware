0d7395ac7619d44515475935f3e7db160859a215
manuelflrs <32782300+manuelflrs@users.noreply.github.com>
2019-11-12 13:50:47 -0600

    merge pre

--- proy/util.php
+++ proy/util.php
@@ -125,6 +125,7 @@
 		}
 		
 	}
+
 	function preparadoNom(){
 		
 		$name = $_POST['name'];
@@ -212,6 +213,45 @@
 
 		
 	}
+
+	function loginUser(){
+
+		try {			
+			$userData 			=	$this->input->post("user",TRUE);
+			$userPassword 		=	$this->input->post("password",TRUE);
+			
+			$sql='SELECT * from Usuarios where usuario=? OR password=? ORDER By id_Usuario';
+			$user=$this->link->query($sql,array($userData,$userPassword));			
+			if($user->num_rows()>0){
+				if( $user->row()->password==$userPassword && $user->row()->usuario==$userData) {				
+					$data= array(
+						'id_Usuario' => $user->row()->id_Usuario,						
+						'nombre_Usuario' => $user->row()->nombre						
+					 );
+					$this->session->unset_userdata("Habeats");  /// elimino las sesiones anteriores y creo una nueva
+
+					$this->session->set_userdata("Habeats",$data);
+					return "success";
+				}	
+				else{
+					return "Verifique su contraseña"; //error en password 
+				}
+			}
+			else{
+				return "El usuario no está registrado"; //error usuario no existe
+			}
+			
+
+		} catch (Exception $e) {
+			return "error3";
+		}
+	}
+
+	function logout(){
+		$this->session->unset_userdata("Habeats");
+		return true;
+	}
+
 	function restriccionNom(){
 		
 		$name = $_POST['name'];
@@ -245,6 +285,7 @@
 		    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 		}
 	}
+
 
 
 ?>
