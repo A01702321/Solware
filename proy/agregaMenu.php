f02fb6b907aa580831f056205836387bd223cc9f
A1365099 <A1365099@users.noreply.github.com>
2019-11-15 13:50:58 -0600

    valida y muestra alert para errores al insertar menus

--- proy/agregaMenu.php
+++ proy/agregaMenu.php
@@ -10,11 +10,20 @@
     require_once("util.php");
     
     if(isset($_POST["nombreMenu"])){
-
-       
-        crearMenu($_POST["nombreMenu"]);
-        header("location:menu.php");
-                   
+         if (empty($_POST["nombreMenu"])) {
+             echo '<script language="javascript">';
+             echo 'alert("Se requiere llenar el campo")';
+             echo '</script>';
+        }  
+       else if (!preg_match("/^[a-zA-Z ]*$/",$_POST["nombreMenu"])) {
+            echo '<script language="javascript">';
+            echo 'alert("Solo se permiten letras y especios")';
+            echo '</script>';
+        }else{
+        	
+            crearMenu($_POST["nombreMenu"]);
+            header("location:menu.php");
+        }
     }
     else{
         include_once("AgregaMenu.html");
