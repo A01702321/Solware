ff158a6539746bcf399838343e070e203cf78ebc
A1365099 <A1365099@users.noreply.github.com>
2019-11-23 15:02:23 -0600

    agrega preparado y relacion con ingredientes

--- proy/agregaPreparado.php
+++ proy/agregaPreparado.php
@@ -15,13 +15,38 @@
         }else{
         	$namep = preg_replace('/\s+/', ' ',$_POST["nombreprep"]);
         	crearPreparado($namep);
-        	for($i = 1; $i <= $_POST["ing1"];$i++){
-        		$ing = preg_replace('/\s+/', ' ',$_POST["ing1"]);
-               $ingrediente = "ing1".$i;
-                agregarIngPreparado($namep, $_POST[$ingrediente]);
+        	/*if(isset($_POST["valorIngredientes"])){
+                    for($i = 0; $i <= $_POST["valorIngredientes"];$i++){
+                        $tipo = "ingrediente".$i;
+
+                        if($_POST[$tipo] != NULL){
+                            echo($_POST[$tipo]);
+                            echo($namep);
+                            agregarIngPreparado($namep,$_POST[$tipo]);
+                        }
+                    }
+                }*/
+
+        	/*for($i = 0; $i <= $_POST["ing".($i+1)];++$i){
+        		echo '<script language="javascript">';
+             	echo 'alert("Se requiere llenar el campo")';
+             	echo '</script>';
+        		$ing = preg_replace('/\s+/', ' ',$_POST["ing".($i+1)]);
+        		echo("size: " . sizeof($_POST["ing".($i+1)]));
+        		echo("ing: " . $ing);
+                agregarIngPreparado($namep, $ing);
                
              }
-        	
+          */
+             foreach ($_POST as $ings) {
+             	# code...
+             	$ing = preg_replace('/\s+/', ' ',$ings);
+             	if($ing != $namep && $ing != ""){
+             		 agregarIngPreparado($namep, $ing);
+             	}
+             }
+
+        	//echo "hola ". $_POST["valorIngredientes"];
             header("location:registroExitoso.php");
         }
     }
