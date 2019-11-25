4b4888d3833522d83c5773c16611f92e4c06d5fe
A1365099 <A1365099@users.noreply.github.com>
2019-11-25 11:24:13 -0600

    ya se pueden agregar acentos y ñ, valida que no se pueda agregar preparado que ya existe

--- proy/agregaPreparado.php
+++ proy/agregaPreparado.php
@@ -8,46 +8,30 @@
              echo 'alert("Se requiere llenar el campo")';
              echo '</script>';
         }  
-        else if (!preg_match("/^[a-zA-Z]+[a-zA-Z ]*+[a-zA-Z]*$/",$_POST["nombreprep"])) {
+        else if (!preg_match("/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+[a-zA-ZáéíóúÁÉÍÓÚñÑ ]*+[a-zA-ZáéíóúÁÉÍÓÚñÑ]*$/",$_POST["nombreprep"])) {
             echo '<script language="javascript">';
             echo 'alert("Solo se permiten letras y espacios \nNo se puede iniciar con espacio")';
             echo '</script>';
         }else{
         	$namep = preg_replace('/\s+/', ' ',$_POST["nombreprep"]);
-        	crearPreparado($namep);
-        	/*if(isset($_POST["valorIngredientes"])){
-                    for($i = 0; $i <= $_POST["valorIngredientes"];$i++){
-                        $tipo = "ingrediente".$i;
+        	if(!existe("preparados","NombrePreparado", $namep, true)){
+	        	crearPreparado($namep);
+	        	
+	             foreach ($_POST as $ings) {
+	             	# code...
+	             	$ing = preg_replace('/\s+/', ' ',$ings);
+	             	if($ing != $namep && $ing != ""){
+	             		 agregarIngPreparado($namep, $ing);
+	             	}
+	             }
 
-                        if($_POST[$tipo] != NULL){
-                            echo($_POST[$tipo]);
-                            echo($namep);
-                            agregarIngPreparado($namep,$_POST[$tipo]);
-                        }
-                    }
-                }*/
-
-        	/*for($i = 0; $i <= $_POST["ing".($i+1)];++$i){
-        		echo '<script language="javascript">';
-             	echo 'alert("Se requiere llenar el campo")';
-             	echo '</script>';
-        		$ing = preg_replace('/\s+/', ' ',$_POST["ing".($i+1)]);
-        		echo("size: " . sizeof($_POST["ing".($i+1)]));
-        		echo("ing: " . $ing);
-                agregarIngPreparado($namep, $ing);
-               
-             }
-          */
-             foreach ($_POST as $ings) {
-             	# code...
-             	$ing = preg_replace('/\s+/', ' ',$ings);
-             	if($ing != $namep && $ing != ""){
-             		 agregarIngPreparado($namep, $ing);
-             	}
-             }
-
-        	//echo "hola ". $_POST["valorIngredientes"];
-            header("location:registroExitoso.php");
+	        	//echo "hola ". $_POST["valorIngredientes"];
+	            header("location:registroExitoso.php");
+	        }else {
+            include_once("header.html");
+            echo("Existe ya un preparado con los datos introducidos");
+            include_once("AgregaPreparado.html");
+        } 
         }
     }
     
