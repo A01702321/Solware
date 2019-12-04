c3c74d5285a2e88843fa40dec806cb6c1e861ebc
Max Burkle <A01702321@itesm.mx>
2019-12-04 16:47:18 -0600

    arreglado login, inicia sesion con user y password de la base de datos

---
+++ proy/login.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+include("util.php");
+
+$pwd = $_POST["Contraseña"];
+$user = $_POST["User"];
+
+if($pwd == getQuery($user)){
+    $_SESSION["User"] = $user;
+	header("location:../proy/menu.php");
+} else {
+	echo "<script> 
+            alert('El usuario o contraseña son incorrectos'); 
+            window.location.href='Login.html'; 
+          </script>";
+}
+
+function getQuery($user) {
+    $bd = connectDB();
+    //$sql = "SELECT Contraseña FROM Usuarios WHERE NombreUsuario='$user'";
+    $reg = mysqli_query( $bd , "SELECT Contraseña FROM Usuarios WHERE NombreUsuario='$user'");
+    $row = mysqli_fetch_assoc($reg);
+
+    return $row["Contraseña"];
+}
+
+?>
